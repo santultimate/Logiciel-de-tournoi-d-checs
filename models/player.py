d03d45a3a588ad34a96ef0f1c6e677f961e31432
santultimate <63077773+santultimate@users.noreply.github.com>
2024-11-13 20:55:19 +0000

    ajouter du fichier menu controller

--- models/player.py
+++ models/player.py
@@ -1,14 +1,13 @@
-import json
-
 class Player:
     def __init__(self, last_name, first_name, birth_date, national_id):
         self.last_name = last_name
         self.first_name = first_name
         self.birth_date = birth_date
         self.national_id = national_id
-        self.score = 0
+        self.score = 0  # Score initial
 
     def to_dict(self):
+        """Convertit un joueur en dictionnaire pour la sauvegarde."""
         return {
             "last_name": self.last_name,
             "first_name": self.first_name,
@@ -19,10 +18,11 @@ class Player:
 
     @classmethod
     def from_dict(cls, data):
+        """Crée un joueur à partir d'un dictionnaire."""
         player = cls(
-            data["last_name"], 
-            data["first_name"], 
-            data["birth_date"], 
+            data["last_name"],
+            data["first_name"],
+            data["birth_date"],
             data["national_id"]
         )
         player.score = data.get("score", 0)
